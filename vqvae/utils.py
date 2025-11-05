cec4c13d21084960d6ca8f8c025e6f4e62bd67ea
shirinrey <s.reyhanian.m@gmail.com>
2025-11-05 13:57:23 +0100

    modified model initialization

--- vqvae/utils.py
+++ vqvae/utils.py
@@ -12,25 +12,26 @@ import vqvae.flat.flat_models as flat_models
 
 def initialize_model(params):
     model_type = params["model_type"]
-    for module in (flat_models):
-        if hasattr(module, model_type):
-            cls = getattr(module, model_type)
-            return cls(
-                in_channel=3,
-                channel=params["latent_channel"],
-                n_res_block=2,
-                n_res_channel=params["latent_channel"] // 2,
-                embed_dim=params.get("embed_dim", None),
-                codebook_dim=params["codebook_dim"],
-                n_embed=params["codebook_size"],
-                decay=params["decay"],
-                rotation_trick=params["rotation_trick"],
-                kmeans_init=params["kmeans_init"],
-                learnable_codebook=params["learnable_codebook"],
-                ema_update=params["ema_update"],
-                threshold_ema_dead_code=params.get("threshold_dead", None),
-            )
-    raise ValueError(f"Model type '{model_type}' not found in available modules.")
+    try:
+        cls = getattr(flat_models, model_type)  # e.g., UnconditionedHVQVAE
+    except AttributeError:
+        raise ValueError(f"Unknown model '{model_type}' in flat_models")
+
+    return cls(
+        in_channel=3,
+        channel=params["latent_channel"],
+        n_res_block=2,
+        n_res_channel=params["latent_channel"] // 2,
+        embed_dim=params.get("embed_dim", None),
+        codebook_dim=params["codebook_dim"],
+        n_embed=params["codebook_size"],
+        decay=params["decay"],
+        rotation_trick=params["rotation_trick"],
+        kmeans_init=params["kmeans_init"],
+        learnable_codebook=params["learnable_codebook"],
+        ema_update=params["ema_update"],
+        threshold_ema_dead_code=params.get("threshold_dead", None),
+    )
 
 def init_seeds(seed=None):
     seed = random.randint(0, 2147483647) if seed is None else seed
